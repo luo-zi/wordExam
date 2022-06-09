d99fe1b63c11d09860a186ee8c5ffe7bd6df7420
luozi <zjw1355528158@outlook.com>
2022-06-10 01:39:35 +0800

    First commit
    Why:
    EN: The English test is about to start, write a tool to practice cramming
    ZH: 二级考马上就要开始了,赶紧写个工具临时练练抱佛脚
    
    How:
    The program will give the user a Chinese word, and then user can type
    the English that means same thing
    
    Tags:
    Add Start
    
    Co-authored-by: Name <zjw1355528158@outlook.com>
    Sponsored-by: Name <zjw1355528158@outlook.com>
    Sponsored-by: Name <zjw1355528158@outlook.com>

---
+++ main.py
@@ -0,0 +1,89 @@
+import random
+
+
+def main(name):
+    """
+    name: 没啥用,懒得删
+    info:
+    读入词汇表文件到列表
+    使用随机索引取出列表项
+    打印中文意思, 等待用户输入英文
+    给出正误反馈
+    退出后结算练习次数和成功率
+
+    ===============================
+    词汇表的格式要求(一行):
+    ------------------
+    英文 中文
+    ------------------
+    英文与中文间隔一个空格, 不得出现前导空格与尾部空格
+    """
+    wordsList=[]
+    success=0
+    failure=0
+
+    with open("./out1.txt","r") as f: #out.txt是EnglishLevelTwo.txt经过几个函数的处理后, 格式良好的词汇表文件
+        l=f.readlines()
+        for x in l:
+            kv=x.split(" ")
+            wordsList.append((kv[0],kv[1]))
+
+    print("根据中文意思写出英文单词")
+    print("输入 '=' 以退出")
+
+
+
+    r=random.randint
+    while True:
+        listItem=wordsList[r(0,len(wordsList)-1)]
+        inputStr=input(listItem[1]+"提示:"+(listItem[0])[0:3]+"\n")
+        if inputStr=="=":
+            break
+        elif inputStr==listItem[0]:
+            print("+ Yeeeees~~~")
+            success+=1
+        else:
+            print("- Noooooooo")
+            failure+=1
+            print(f"Is {listItem[0]}")
+
+    successRate=0 if success==0 else round(success/(success+failure)*100,1)
+
+    print(f"共练习:{success+failure}次,成功率:{successRate}%")
+
+
+
+
+#
+# def FormatTheText(name):
+#     # 在下面的代码行中使用断点来调试脚本。
+#     outText = ""
+#     with open("./out1.txt","r") as f:
+#         fList=f.readlines()
+#         outList=list(map(StripSpace,fList))
+#         for x in outList:
+#             outText+=x
+#     with open("./out1.txt","x") as f:
+#         f.write(outText)
+#         f.flush()
+#
+# def StripSpace(s:str):
+#     r=""
+#     space=False
+#     for x in s:
+#         if space and x ==" ": #如果已经遇到空格,则跳过
+#             continue
+#         if x==" ":
+#             space=True
+#         r+=x
+#     return  r
+
+if __name__ == '__main__':
+    main('PyCharm')
+
+
+
+
+
+
+
